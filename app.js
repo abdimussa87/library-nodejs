f505fbbeabfa9e5e898f93dc120eedcaaa58f76b
Abdi Mussa <abdimussa87@gmail.com>
2020-05-27 15:41:08 +0300

    Added index.ejs template

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
-var express=  require('express');
-var chalk = require('chalk');
-var morgan = require('morgan');
-var path = require('path');
-var debug = require('debug')('app');
+const express = require('express');
+const chalk = require('chalk');
+const morgan = require('morgan');
+const path = require('path');
+const debug = require('debug')('app');
 
-var app = express();
+const port = process.env.PORT || 8080;
+const app = express();
 
 app.use(morgan('tiny'));// acts like a middleware and console logs the request header
-app.use(express.static(path.join(__dirname,'/assets/')));
-app.use('/js',express.static(path.join(__dirname,'node_modules/bootstrap/dist/js')));
-app.use('/css',express.static(path.join(__dirname,'node_modules/bootstrap/dist/css')));
-app.use('/js',express.static(path.join(__dirname,'node_modules/jquery/dist')));
+app.use(express.static(path.join(__dirname, '/assets/')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
+
+// setting template engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 
-
-
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'views/index.html'));
+app.get('/', (req, res) => {
+  res.render('index', { myList: ['a', 'b'], title: 'My Library' });
 });
 
-app.listen(8080,()=>{
-    debug(`listening on port ${chalk.green(8080)} ` );
+app.listen(port, () => {
+  debug(`listening on port ${chalk.green(port)} `);
 });
